7e44ee2dc83f911109cfdb9fcdd3a73fd381281d
AntonKechashin <atk@sitecore.net>
2017-10-13 11:05:53 +0300

    Initial commit.

---
+++ src/Sitecore.Support.393977/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.393977")]
+[assembly: AssemblyProduct("Sitecore.Support.393977")]
+[assembly: ComVisible(false)]
---
+++ src/Sitecore.Support.393977/Shell/Framework/Commands/ArchiveVersion.cs
@@ -0,0 +1,27 @@
+﻿namespace Sitecore.Support.Shell.Framework.Commands
+{
+    using System.Collections.Specialized;
+    using System.Web;
+    using Sitecore.Data;
+    using Sitecore.Data.Items;
+    using Sitecore.Diagnostics;
+    using Sitecore.Shell.Framework.Commands;
+
+    public class ArchiveVersion : Sitecore.Shell.Framework.Commands.ArchiveVersion
+    {
+        public override void Execute(CommandContext context)
+        {
+            Assert.ArgumentNotNull(context, "context");
+            if (context.Items.Length == 1)
+            {
+                Item item = context.Items[0];
+                NameValueCollection nameValueCollection = new NameValueCollection();
+                nameValueCollection["database"] = item.Database.Name;
+                nameValueCollection["item"] = item.ID.ToString();
+                nameValueCollection["language"] = item.Language.Name;
+                nameValueCollection["version"] = ItemUri.Parse(HttpContext.Current.Request.Form["__CurrentItem"]).Version.ToString();
+                Sitecore.Context.ClientPage.Start(this, "Run", nameValueCollection);
+            }
+        }
+    }
+}
